0031ee2ba3c9d4fd7a8ddb2f34c62ef288ae0153
mateus-f-torres <mateusfigtorres@gmail.com>
2020-03-17 11:19:36 -0300

    test(specs): write empty tests
    Start TDD process by writting basic falling tests

--- src/components/App/App.spec.js
+++ src/components/App/App.spec.js
@@ -47,3 +47,41 @@ describe('App', () => {
     })
   })
 })
+
+// NOTE: integration
+describe('Search Field', function() {
+  it('should filter the product list with given text', function() {})
+})
+
+// NOTE:
+//  2 - ID are insertion order based (not written)
+//  3 - show error on wrong input value
+//  4 - show error on 0 stock value
+//  7 - reuse deleted IDs
+describe('New Product Form', function() {
+  // NOTE: unit
+  it('should NOT allow an unnamed product creation', function() {})
+  it('should NOT allow a repeated product creation', function() {})
+  it('should NOT allow a numeric-only named product creation', function() {})
+  it('should NOT allow a zero or negative stock product creation', function() {})
+  it('should NOT allow a non-numeric stock product creation', function() {})
+  it('should NOT allow a zero or negative price product creation', function() {})
+  it('should NOT allow a non-numeric price product creation', function() {})
+
+  // NOTE: integration
+  it('should add a new product to the list when created', function() {})
+  it('should reuse any vacant ID when inserting a new product in the list', function() {})
+})
+
+// NOTE:
+//  1 - changes should be locally saved (page refresh wont reset list)
+//  5 - changing stock to 0 deletes entry
+//  6 - total value changes with stock|price modifications
+describe('Product List', function() {
+  it('should remove a zeroed stock quantity entry', function() {})
+  it('should alter total value when stock AND|OR price is modified', function() {})
+  it('should remove an entry when delete button is clicked', function() {})
+  it('should allow users to reorder the list by dragging items', function() {})
+  it('should reorder the list when headers are clicked', function() {})
+  it('should locally save list state (page refresh wont reset list)', function() {})
+})
