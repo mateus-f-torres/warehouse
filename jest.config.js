109aef18eccfbffd5f305761d634f93e42989eca
mateus-f-torres <mateusfigtorres@gmail.com>
2020-03-24 10:04:56 -0300

    refactor(storybook, jest): remove storybook and jest

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  setupFilesAfterEnv: ['<rootDir>/__tests__/tl.setup.js'],
-  testPathIgnorePatterns: ['tl.setup.js'],
-  moduleNameMapper: {
-    '\\.(css|sass|scss|less)$': '<rootDir>/__mocks__/styleStub.js',
-    '\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/__mocks__/imageStub.js',
-  },
-}
