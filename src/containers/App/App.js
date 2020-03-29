716623fa9b63a683e49ba5a2f8547684d9d46208
mateus-f-torres <mateusfigtorres@gmail.com>
2020-03-29 17:38:36 -0300

    refactor(app): cleanup files

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,20 +1,62 @@
 import React from 'react'
-import ProductsPage from '../ProductsPage/ProductsPage'
+import ProductList from '../../components/ProductList'
+import ProductForm from '../../components/ProductForm'
 import useDatabase from '../../hooks/useDatabase'
-import './App.css'
+
+export function normalizeString(str) {
+  return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+}
 
 function App() {
   const database = useDatabase()
+  const [filter, changeFilter] = React.useState(/./)
+
+  function handleSearch(e) {
+    const input = e.target.value.trim()
+    if (input) {
+      const normalizedInput = normalizeString(input)
+      const newFilter = new RegExp(normalizedInput, 'ig')
+      changeFilter(newFilter)
+    } else {
+      changeFilter(/./)
+    }
+  }
+
+  const [userIsAddingProduct, toggleUserIsAddingProduct] = React.useState(false)
+
+  function handleNewProductSubmission(product) {
+    database.addProduct(product)
+    toggleUserIsAddingProduct(!userIsAddingProduct)
+  }
+
+  function repeatedProductCheck(productName) {
+    return (
+      database.list.find(({product}) => product == productName) !== undefined
+    )
+  }
 
   return (
-    <div className="container">
-      <ProductsPage
-        list={database.list}
+    <div>
+      <p>Hello World</p>
+      <p>brought to you by Mateus F Torres</p>
+      <input placeholder="Buscar" onChange={handleSearch} />
+      <button onClick={() => toggleUserIsAddingProduct(!userIsAddingProduct)}>
+        Adicionar novo produto
+      </button>
+      {userIsAddingProduct && (
+        <ProductForm
+          closeModal={() => toggleUserIsAddingProduct(false)}
+          checkForRepeatedProduct={repeatedProductCheck}
+          onSubmission={handleNewProductSubmission}
+        />
+      )}
+      <ProductList
+        filter={filter}
         sort={database.sort}
+        list={database.list}
         reOrder={database.reOrder}
-        addProduct={database.addProduct}
-        updateProduct={database.updateProduct}
-        removeProduct={database.removeProduct}
+        onEdit={database.updateProduct}
+        onDelete={database.removeProduct}
       />
     </div>
   )
