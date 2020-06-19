374637c72e7e9924c8a0e9899a6364159efed064
mateus-f-torres <mateusfigtorres@gmail.com>
2020-06-19 13:30:48 -0300

    refactor(indexeddb): refactor database utils and hooks to use idb lib
    In this commit the delay and random error are off
    Cleanup a lot of code with the help of idb
    Change some methods names to better match standard API

--- src/utils/indexedDB/createDatabase.js
+++ src/utils/indexedDB/createDatabase.js
@@ -1,25 +1,81 @@
-import composeDatabase from './composeDatabase'
+import {openDB} from 'idb'
 
-function createDatabase({name, version, store, key}) {
-  return new Promise((resolve, reject) => {
-    const request = window.indexedDB.open(name, version)
-    request.onerror = (e) => reject(e)
-
-    request.onupgradeneeded = (e) => {
-      const db = e.target.result
-
-      const storeObj = db.createObjectStore(store, {keyPath: key})
-
-      storeObj.transaction.oncomplete = () => {
-        resolve(composeDatabase(db, store))
-      }
-    }
-
-    request.onsuccess = (e) => {
-      const db = e.target.result
-      resolve(composeDatabase(db, store))
-    }
+async function createDatabase({name, version, store, key}) {
+  const db = await openDB(name, version, {
+    upgrade(database) {
+      return database.createObjectStore(store, {keyPath: key})
+    },
   })
+
+  return {
+    add: makeAdd(db, store),
+    put: makePut(db, store),
+    delete: makeDelete(db, store),
+    getAll: makeGetAll(db, store),
+    putAll: makePutAll(db, store),
+    clearAll: makeClearAll(db, store),
+  }
 }
+
+function makeAdd(db, store) {
+  return (data) => db.add(store, data)
+}
+
+function makePut(db, store) {
+  return async (key, data) => {
+    const tx = db.transaction(store, 'readwrite')
+
+    const oldData = await tx.store.get(key)
+    const newData = Object.assign({}, oldData, data)
+
+    await tx.store.put(newData)
+
+    return newData
+  }
+}
+
+function makeDelete(db, store) {
+  return (key) => db.delete(store, key)
+}
+
+function makeGetAll(db, store) {
+  return () => db.getAll(store)
+}
+
+function makePutAll(db, store) {
+  return (data) => {
+    const tx = db.transaction(store, 'readwrite')
+
+    for (const d of data) {
+      tx.store.put(d)
+    }
+
+    return tx.done
+  }
+}
+
+function makeClearAll(db, store) {
+  return () => db.clear(store)
+}
+
+/*
+function delayFor(time) {
+  return new Promise(createTimeout(time))
+}
+
+function createTimeout(time) {
+  return function (done) {
+    window.setTimeout(done, time)
+  }
+}
+
+function makeGetAll(db, store) {
+  return () => {
+    // await delayFor(5000)
+    // fake.throwRandomError()
+    return db.getAll(store)
+  }
+}
+*/
 
 export default createDatabase
