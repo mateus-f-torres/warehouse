bde6acfe71c6578280ab9333d1f8ce579692e1e3
mateus-f-torres <mateusfigtorres@gmail.com>
2020-06-20 10:22:48 -0300

    test(random_items.js): start writting specs

---
+++ cypress/integration/random_items.js
@@ -0,0 +1,21 @@
+describe('Warehouse should', function () {
+  before(() => cy.login())
+
+  it('allow users to create a single random item', function () {
+    // find and click OPTIONS button
+    // find and click on "add random item"
+    // find and verify that an item was added
+
+    // initial standard failing assertion
+    assert.isOk(false, 'Specs need to be written')
+  })
+
+  it('allow users to create multiple random items', function () {
+    // find and click OPTIONS button
+    // find and click on "populate warehouse"
+    // find and verify that x items were added
+
+    // initial standard failing assertion
+    assert.isOk(false, 'Specs need to be written')
+  })
+})
