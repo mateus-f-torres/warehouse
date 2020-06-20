43189cfa5405f01bc5ea2627b5ef153b6e011377
mateus-f-torres <mateusfigtorres@gmail.com>
2020-06-20 13:44:48 -0300

    test(random_items.js): pass spec

--- cypress/integration/random_items.js
+++ cypress/integration/random_items.js
@@ -4,12 +4,15 @@ describe('Warehouse should', function () {
   it('allow users to create a single random item', function () {
     cy.findByTestId('options').click()
     cy.findByText('Add 1 random item').click().type('{esc}')
+
     cy.findAllByTestId('item').should('have.length', 1)
   })
 
   it('allow users to create multiple random items', function () {
     cy.findByTestId('options').click()
-    cy.findByText('Add 10 random items').click().type('{esc}')
-    cy.findAllByTestId('item').should('have.length', 10)
+    cy.findByText('Add 5 random items').click().type('{esc}')
+
+    // adding 1 from from the previous test
+    cy.findAllByTestId('item').should('have.length', 6)
   })
 })
