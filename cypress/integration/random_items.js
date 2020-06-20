c552e8b5167e0eb25bdedaeecb20f95bb2ec8dde
mateus-f-torres <mateusfigtorres@gmail.com>
2020-06-20 13:45:35 -0300

    style(random_items.js): change wording on spec

--- cypress/integration/random_items.js
+++ cypress/integration/random_items.js
@@ -1,14 +1,14 @@
 describe('Warehouse should', function () {
   before(() => cy.login())
 
-  it('allow users to create a single random item', function () {
+  it('allow users to add a single random item', function () {
     cy.findByTestId('options').click()
     cy.findByText('Add 1 random item').click().type('{esc}')
 
     cy.findAllByTestId('item').should('have.length', 1)
   })
 
-  it('allow users to create multiple random items', function () {
+  it('allow users to add multiple random items', function () {
     cy.findByTestId('options').click()
     cy.findByText('Add 5 random items').click().type('{esc}')
 
